a1922f89d0024b11dd28efbc4b17e0febd5ff868
Andrey Kurzantsev <hawkeye54123@gmail.com>
2022-01-07 01:07:40 +0200

    created front layout page with ejs

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
-'use strict';
+"use strict";
 
-import express from 'express';
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import path from "path";
+dotenv.config({ path: "var.env" });
 
-const PORT = 5000;
-
+const PORT = process.env.PORT;
 const app = express();
+const __dirname = path.resolve();
+
+app.use(morgan("tiny"));
 app.use(express.json());
+//app.set("views", path.resolve(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use('/css', express.static(path.resolve(__dirname, "sources/css")));
+app.use('/img', express.static(path.resolve(__dirname, "sources/img")));
+app.use('/js', express.static(path.resolve(__dirname, "sources/js")));
+
 app.get('/', (req, res) => {
     try {
-        res.status(200).json('Server works');
+        res.status(200).render('index.ejs');
     } catch (error) {
         res.status(500).json(error.message);
     }
@@ -19,7 +32,7 @@ let startApp = async () => {
     try {
         app.listen(PORT, () => console.log(`SERVER STARTED ON PORT ${PORT}`));
     } catch (error) {
-        console.log('ERROR');
+        console.log("ERROR");
     }
 };
 startApp();
